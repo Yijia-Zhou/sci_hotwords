e8ec816e5dc23afcbe3cc45a213238d10b08e464
aaakk <951412142@qq.com>
2022-11-08 08:16:54 +0800

    adding query result display

--- miniprogram/pages/query/query.js
+++ miniprogram/pages/query/query.js
@@ -5,20 +5,59 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isShowResultView: false,
+    searchResultArr : [],
+    allDictionary :[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+
+    console.log("query on load")
     wx.setNavigationBarTitle({title: '词汇查询'})
-  },
 
-  onQuery(){
+    var useDictList = wx.getStorageSync('dictInfo').useDictList
+
+    console.log(useDictList)
+
+    var allDictionary = []
     
+    for(var useDict in useDictList)
+    {
+      allDictionary.push.apply(allDictionary, wx.getStorageSync(useDictList[useDict]))
+    }
+
+    this.setData({
+      allDictionary: allDictionary
+    })
   },
 
+  onQuery(e){
+    var resultArr = []
+    for(var i = 0; i < 5; i++)
+    {
+      resultArr.push(this.data.allDictionary[i]._id)
+    }
+    console.log(resultArr)
+
+    this.setData({
+      isShowResultView : true,
+      searchResultArr : resultArr
+    })
+
+    console.log(this.data.isShowResultView)
+
+  },
+
+  onCancel(){
+    this.setData({
+      isShowResultView : false,
+      searchResultArr : []
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
