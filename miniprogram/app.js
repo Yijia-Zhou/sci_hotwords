d64edf9ab76bdf32e2c57fb3b4421e066a1a761d
为之奈何 <oncwnuMs4T_wQx6PO5HHuGPKdpk4@git.weixin.qq.com>
2023-06-09 16:31:47 +0800

    计算显示长度的方法增加对大写字母的适配

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -235,7 +235,7 @@ App({
   count_display_length: function (word) {
     let res = 0
     for (let char in word) {
-      switch(word[char]) { // 用法参考 https://blog.csdn.net/tel13259437538/article/details/83314965
+      switch(word[char].toLowerCase()) { // 用法参考 https://blog.csdn.net/tel13259437538/article/details/83314965
         case 'i':
         case 'j':
         case 'l':
@@ -245,6 +245,7 @@ App({
         case 'f':
         case 'r':
         case 't':
+        case '-':
           res += 10
           break
         case 'm':
@@ -254,7 +255,10 @@ App({
         default:
           res += 14
       }
-      if (escape(word[char]).indexOf("%u") >= 0) { // 判断方法来自 https://juejin.cn/post/6844903745583579149
+      if (/^[A-Z]$/.test(word[char])) {
+        res += 5  // 如果是大写字母则额外+5
+      }
+      else if (escape(word[char]).indexOf("%u") >= 0) { // 判断方法来自 https://juejin.cn/post/6844903745583579149
         res += 10  // 如果不是英文字符则额外+10（默认当中文(长度24)处理）
       }
     }
