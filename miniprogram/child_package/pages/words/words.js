a010d76d6afb90c29a04a16ec6f4df81f8528933
为之奈何 <Yijia-Zhou@users.noreply.github.com>
2023-08-12 16:18:06 +0800

    [重置版]picker第一列(cluster选择)相关逻辑实现 Feature/#60 menu page first column selector
    * add utils
    
    * #60 usedict update with first column change
    
    * Modify cluster
    
    * Make distinguish between clusters
    
    * fix display module
    
    ---------
    
    Co-authored-by: aaakk <951412142@qq.com>

--- miniprogram/child_package/pages/words/words.js
+++ miniprogram/child_package/pages/words/words.js
@@ -82,7 +82,7 @@ Page({
         return Math.min(40, 500/(len+1))
       }
       let diff_showcase_here = new Array()
-      let words = app.globalData.dictInfo.dictNames.生命科学[dataDict.getUseDict()].diff_showcase
+      let words = app.globalData.dictInfo.dictNames[dataDict.getUseCluster()][dataDict.getUseDict()].diff_showcase
       for (let i in words) {
         diff_showcase_here.push({
           word: words[i],
@@ -186,6 +186,7 @@ Page({
     let dataDict = this.data.dictionary
     
     this.updateUseMode(dictInfo.useMode)
+    dataDict.updateUseCluster(dictInfo.useCluster)
     dataDict.updateUseDict(dictInfo.useDict)
     
     if(dataDict.isFilterEnabled() && dictInfo.hasOwnProperty('no_high_school'))
@@ -214,7 +215,7 @@ Page({
     console.log("words onLoad start")
     console.log(dictInfo)
 
-    wx.setNavigationBarTitle({title: '生命科学 - ' + dictInfo.useDict})
+    wx.setNavigationBarTitle({title: dictInfo.useCluster + ' - ' + dictInfo.useDict})
 
     this.initialDictionary(dictInfo)
   },
@@ -276,7 +277,7 @@ Page({
     if(dataDict.showCoreWordNum() && dataDict.isCoreNumUpdated())
     {
       let coreNum = dataDict.getCoreWordNum()
-      wx.setNavigationBarTitle({title: '生命科学 - ' + dataDict.getUseDict() + 
+      wx.setNavigationBarTitle({title: dataDict.getUseCluster() + ' - ' + dataDict.getUseDict() + 
                                 ' - 核心' + coreNum + '00词'})
       let _this = this
       switch (dataDict.getUseMode()) {
