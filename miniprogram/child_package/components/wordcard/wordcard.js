1ca9ac76c97f2649b41f204ad063663e984df415
为之奈何 <oncwnuMs4T_wQx6PO5HHuGPKdpk4@git.weixin.qq.com>
2023-09-05 19:03:17 +0800

    衍生词对应的释义前面加个“[机翻]”

--- miniprogram/child_package/components/wordcard/wordcard.js
+++ miniprogram/child_package/components/wordcard/wordcard.js
@@ -100,7 +100,7 @@ Component({
       var deri_obj = this.properties.word.deris[event.target.id.substr(4,1)]
       wx.showModal({
         title: deri_obj.word,
-        content: (Boolean(deri_obj.bing)?deri_obj.bing:"暂无释义") + '\r\n 出现次数：' + String(deri_obj.count), 
+        content: (Boolean(deri_obj.bing)?'[机翻]'+deri_obj.bing:"暂无释义") + '\r\n 出现次数：' + String(deri_obj.count), 
         showCancel: false
       })
     },
@@ -110,7 +110,7 @@ Component({
       var deri_obj = this.properties.word.deris[derisIndex]
       wx.showModal({
         title: deri_obj.word,
-        content: (Boolean(deri_obj.bing)?deri_obj.bing:"暂无释义") + '\r\n 出现次数：' + String(deri_obj.count), 
+        content: (Boolean(deri_obj.bing)?'[机翻]'+deri_obj.bing:"暂无释义") + '\r\n 出现次数：' + String(deri_obj.count), 
         showCancel: false
       })
     },
